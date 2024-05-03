db1e0efa78b220089c9730bbd1ce969353d3eb52
minseo <choms1357@gmail.com>
2024-05-03 22:49:46 +0900

    after-styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import Card from './Card';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  
+  box-sizing : border-box;
+
+  width : 100%;
+  height : 100vh;
+
+  padding : 0 10%;
+  
+  display : flex;
+  flex-direction : column;
+  align-items:center;
+  justify-content: center;
+`;
+
+const Title= styled.div`
+  font-size: 2rem; //기본 1rem
+  font-weight: 600;
+  width: 100%;
+`;
+
+const Subtitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+  width:100%;
+`;
 
 function App() {
   return (
     <>
-      <div>제목</div>
-      <div>한 줄 요약</div>
+    <Wrapper>
+      
+      <Title>나만의 PLAYLISTS</Title>
+      <Subtitle>▶️ 가장 마음에 드는 가사 한 줄</Subtitle>
       <Card subject="실리카겔" discription="kyo야 사랑을 해봤니"/>
       <Card subject="잔니비" discription="초록을 거머쥔 우리는 여름으로"/>
       <Card subject="검정치마" discription="난 너랑 있는게 제일 좋아"/>
+    </Wrapper>
     </>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,34 @@
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+
+    margin: 10px 0;
+    padding : 10px;
+
+    width:100%;
+    height:100px;
+
+    border:5px solid yellowgreen;
+    border-radius: 10px;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+`;
+
+const Subject = styled.div`
+    font-size: 1.5rem;
+`;
+
+
 const Card = ({subject, discription}) => {
     return(
-        <div>
-            <div>{subject}</div>
-            <div>{discription}</div>
-        </div>  
+        <Wrapper>
+            <Subject>{subject}</Subject>
+            <div>❤️{discription}</div>
+        </Wrapper>
+        
     );
 };
 
